a23608533ec58b31ee558e8c3dbcff5ac6f53ce8
gbarron2014 <gbarron@utng.edu.mx>
2026-09-24 15:40:16 -0600

    Mi primer metodo de recursividad

---
+++ datos/unidad1/recursividad/Recursividad.java
@@ -0,0 +1,21 @@
+package datos.unidad1.recursividad;
+
+public class Recursividad {
+
+	public static void saludo(int total) {
+		
+		if(total <= 0) //condicion Base
+			return;
+	
+		else {  // condicion recursiva o repetitiva
+			System.out.println("Hola");
+			saludo(total-1);	
+		}	
+	}
+
+
+	public static void main(String[] a) {
+		saludo(10);
+	}
+
+}
