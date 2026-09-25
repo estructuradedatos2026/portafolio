753f6944c20815753bed3687e1e663363ce095b0
gbarron2014 <gbarron@utng.edu.mx>
2026-09-25 14:08:13 -0600

    Se agrega cuenta regresiva

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -13,9 +13,26 @@ public class Recursividad {
 		}	
 	}
 
+	/**
+	* Función que realiza cuenta regresiva de acuerdo a un 
+	* valor entero
+	* Fecha: 25 Septiembre
+	* Autor: Gabriel Barrón
+	*/
+	public static void cuentaRegresiva(int n) {
+
+		if(n < 1) { // Condición Base
+			return;
+		} else {
+			System.out.print(n + " ");
+			cuentaRegresiva(n-1);
+		}
+	}
+
 
 	public static void main(String[] a) {
-		saludo( "Gabriel", 100);
+		//saludo( "Gabriel", 100);
+		cuentaRegresiva(100);
 	}
 
 }
