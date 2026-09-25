fca0ac1fb15650b55f46b82649157f98f500706c
gbarron2014 <gbarron@utng.edu.mx>
2026-09-25 13:43:17 -0600

    Aumente un parámetro al metodo saludo

--- datos/unidad1/recursividad/Recursividad.java
+++ datos/unidad1/recursividad/Recursividad.java
@@ -2,20 +2,20 @@ package datos.unidad1.recursividad;
 
 public class Recursividad {
 
-	public static void saludo(int total) {
+	public static void saludo(String nombre, int total) {
 		
 		if(total <= 0) //condicion Base
 			return;
 	
 		else {  // condicion recursiva o repetitiva
-			System.out.println("Hola");
-			saludo(total-1);	
+			System.out.println("Hola " + nombre);
+			saludo(nombre, total-1);	
 		}	
 	}
 
 
 	public static void main(String[] a) {
-		saludo(10);
+		saludo( "Gabriel", 100);
 	}
 
 }
